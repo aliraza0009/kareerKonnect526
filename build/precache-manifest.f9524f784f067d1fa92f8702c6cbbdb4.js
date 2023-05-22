e6702542af1e87780d35e1c97978f55ebf3b5bcc
aliraza0009 <araza52660@gmail.com>
2023-05-22 11:28:21 +0500

    first commit

---
+++ build/precache-manifest.f9524f784f067d1fa92f8702c6cbbdb4.js
@@ -0,0 +1,338 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d094b5070ed8aa5125f",
+    "url": "/KareerKonnect-Frontend/static/css/main.8f40e55d.chunk.css"
+  },
+  {
+    "revision": "0d094b5070ed8aa5125f",
+    "url": "/KareerKonnect-Frontend/static/js/main.0d094b50.chunk.js"
+  },
+  {
+    "revision": "da6c61ad2bab55d5383f",
+    "url": "/KareerKonnect-Frontend/static/css/1.2ac33929.chunk.css"
+  },
+  {
+    "revision": "da6c61ad2bab55d5383f",
+    "url": "/KareerKonnect-Frontend/static/js/1.da6c61ad.chunk.js"
+  },
+  {
+    "revision": "d325a46c444182142223",
+    "url": "/KareerKonnect-Frontend/static/js/runtime~main.d325a46c.js"
+  },
+  {
+    "revision": "0abc9737c2012cffb3fcb189753cdd13",
+    "url": "/KareerKonnect-Frontend/static/media/logo.0abc9737.png"
+  },
+  {
+    "revision": "2e0847ba0b1e237882854e8dab9da109",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-300.2e0847ba.eot"
+  },
+  {
+    "revision": "f6ef809fb833dbf1abcd5b35ab1576e5",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-300.f6ef809f.woff2"
+  },
+  {
+    "revision": "729c04d1fd63b2f3e9d4bc8742aa2d15",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-300.729c04d1.svg"
+  },
+  {
+    "revision": "dad8b32d6402d45efbd9f2a8ee6f203a",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-300.dad8b32d.woff"
+  },
+  {
+    "revision": "81f9f9329c89f478b8f3cf8bc58f4c5c",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-300italic.81f9f932.eot"
+  },
+  {
+    "revision": "5e4a0e72a17dffda2e8c2af3af358110",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-300italic.5e4a0e72.woff2"
+  },
+  {
+    "revision": "65bbf50d80ffbd75536abea87a4962db",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-300.65bbf50d.ttf"
+  },
+  {
+    "revision": "ffcf1f1c66a6661dabcf9a52581186d5",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-300italic.ffcf1f1c.woff"
+  },
+  {
+    "revision": "f00ab8be77768ee0afe76d8fa105e7ee",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-regular.f00ab8be.eot"
+  },
+  {
+    "revision": "00b670ebdb4b1c61f8810fae255cb963",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-300italic.00b670eb.ttf"
+  },
+  {
+    "revision": "ce0c9ae08840a0b43bccb9f5a86e155d",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-regular.ce0c9ae0.woff2"
+  },
+  {
+    "revision": "1fce830e6112511a77108832e13172fd",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-regular.1fce830e.woff"
+  },
+  {
+    "revision": "936aaefc2efb764f3f52c0e9f34ef749",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-italic.936aaefc.woff2"
+  },
+  {
+    "revision": "bbd3f7418bf2a0875103b1bb23159829",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-300italic.bbd3f741.svg"
+  },
+  {
+    "revision": "4711bbc2872e5e734fe5d84804f24967",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-regular.4711bbc2.ttf"
+  },
+  {
+    "revision": "a2ed588ba986f48bab85d75d51fd80fb",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-regular.a2ed588b.svg"
+  },
+  {
+    "revision": "9741803691bc57d55082d53237380709",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-italic.97418036.eot"
+  },
+  {
+    "revision": "055f0699f59e8b1eb28270b304c5d11b",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-italic.055f0699.woff"
+  },
+  {
+    "revision": "506db506cbae0183e1b2c50d069d3769",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-italic.506db506.ttf"
+  },
+  {
+    "revision": "12a5a771441907d9570020c1717158c3",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-500.12a5a771.eot"
+  },
+  {
+    "revision": "cfbff65d7988c6b614e70939c6ab2dec",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-italic.cfbff65d.svg"
+  },
+  {
+    "revision": "08609a017d830988630ee1b38a7ef71a",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-500.08609a01.woff2"
+  },
+  {
+    "revision": "0eb2e0a61fc86d1d9b41a5357b8f88fb",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-500.0eb2e0a6.svg"
+  },
+  {
+    "revision": "0261e08bd22d9f91c1d277cd4874ec95",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-500.0261e08b.woff"
+  },
+  {
+    "revision": "a43a4a36fcfe4d073146b54490eec662",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-500italic.a43a4a36.woff2"
+  },
+  {
+    "revision": "fabb1cda13707046efa0f7238555cc01",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-500italic.fabb1cda.woff"
+  },
+  {
+    "revision": "9972bab7961b40e7da377f55c418e215",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-500italic.9972bab7.ttf"
+  },
+  {
+    "revision": "693057428838523377f5c2da26f44899",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-600.69305742.eot"
+  },
+  {
+    "revision": "22b7688b2941298f2afd730763ef61d9",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-500.22b7688b.ttf"
+  },
+  {
+    "revision": "b4db96df5ce9900b0536fdd0ccf2bd40",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-500italic.b4db96df.svg"
+  },
+  {
+    "revision": "d01ac68c4747e41a359cd94cecc29f9f",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-600.d01ac68c.woff2"
+  },
+  {
+    "revision": "02faed9f2938bbba57514dfbea590d56",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-600.02faed9f.woff"
+  },
+  {
+    "revision": "b01250b38f0ef1dc59107894d37711ec",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-600.b01250b3.ttf"
+  },
+  {
+    "revision": "7aca43b708f8dde583fbaa9bbbf8e1f5",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-500italic.7aca43b7.eot"
+  },
+  {
+    "revision": "d9f0e4b31953d6440559059230442358",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-700.d9f0e4b3.woff2"
+  },
+  {
+    "revision": "319537c2e34754b8482dc10b5a5a678f",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-700.319537c2.eot"
+  },
+  {
+    "revision": "c2b154f61de0c3278da5cfb2a3440371",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-600.c2b154f6.svg"
+  },
+  {
+    "revision": "d1d7b85dc76bad1a967fc842d110262c",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-700.d1d7b85d.woff"
+  },
+  {
+    "revision": "44bb1bc773b816dfb13a88f43d0d0476",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-700.44bb1bc7.ttf"
+  },
+  {
+    "revision": "ef75019674fb2adad506e031bd14331e",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-700italic.ef750196.eot"
+  },
+  {
+    "revision": "da80a603d0220f8e5bb3aaf0e5b42763",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-700italic.da80a603.woff2"
+  },
+  {
+    "revision": "d03f7611353f6f9e728b329d982b7660",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-700italic.d03f7611.woff"
+  },
+  {
+    "revision": "0470693a3857401dad7c0ca10db65617",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-700italic.0470693a.ttf"
+  },
+  {
+    "revision": "a02d5c2b023be148d8d23d3b877d4745",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-700.a02d5c2b.svg"
+  },
+  {
+    "revision": "2f8f4d43aac53d1b881fcd50b08ac5e8",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-800.2f8f4d43.eot"
+  },
+  {
+    "revision": "6f22cd6faf712f6973ee0f38cd38c0e6",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-800.6f22cd6f.woff2"
+  },
+  {
+    "revision": "fabfc4152fe487c9d380365b4dc38a12",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-700italic.fabfc415.svg"
+  },
+  {
+    "revision": "99a9e7bac31ddfc2b58e0025f76c65d1",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-800.99a9e7ba.woff"
+  },
+  {
+    "revision": "97ae8f0f4c3d743a27496689665416bc",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-800.97ae8f0f.ttf"
+  },
+  {
+    "revision": "cc33beefca3fdaae55ac852f32278166",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-800italic.cc33beef.eot"
+  },
+  {
+    "revision": "acbf8d91de92514c5111f02cb26854a9",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-800italic.acbf8d91.woff2"
+  },
+  {
+    "revision": "5c8a310a6164fe0dfec074a89d94f181",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-800italic.5c8a310a.woff"
+  },
+  {
+    "revision": "0aca950ff11dc2a7183a44780d9278a8",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-800.0aca950f.svg"
+  },
+  {
+    "revision": "e85ba4fc01dc62bf49b51b337b710ca8",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-800italic.e85ba4fc.ttf"
+  },
+  {
+    "revision": "f53d38d7a9ce9fb32f278410609cd7d4",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-900.f53d38d7.woff2"
+  },
+  {
+    "revision": "480a646050fb8b991ebff5543ee756b0",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-900.480a6460.eot"
+  },
+  {
+    "revision": "2e8395d21da0e8175146e8823daab8c1",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-800italic.2e8395d2.svg"
+  },
+  {
+    "revision": "1b8eddbe62c11ea9eb4f407ecd897827",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-900.1b8eddbe.woff"
+  },
+  {
+    "revision": "11a038ba2433f905145fc43dfd1dd60b",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-900.11a038ba.svg"
+  },
+  {
+    "revision": "11c0d35b23006eefd198bb52e3e617f9",
+    "url": "/KareerKonnect-Frontend/static/media/poppins-v5-latin-900.11c0d35b.ttf"
+  },
+  {
+    "revision": "9c70885b385cb3965e494c67856fc175",
+    "url": "/KareerKonnect-Frontend/static/media/card-image.9c70885b.png"
+  },
+  {
+    "revision": "ab2f0e2d890c0c7c0cb98f62b18656ff",
+    "url": "/KareerKonnect-Frontend/static/media/service41.ab2f0e2d.jpg"
+  },
+  {
+    "revision": "8e3b734ad59464beb7994605bf451e9e",
+    "url": "/KareerKonnect-Frontend/static/media/Frontend.8e3b734a.jpg"
+  },
+  {
+    "revision": "c7545a20264eed4604d45cedad9d29ea",
+    "url": "/KareerKonnect-Frontend/static/media/Backend.c7545a20.png"
+  },
+  {
+    "revision": "f1a39955e3b88aba87205c43bdf94316",
+    "url": "/KareerKonnect-Frontend/static/media/Mongodb.f1a39955.jpg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/KareerKonnect-Frontend/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/KareerKonnect-Frontend/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/KareerKonnect-Frontend/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/KareerKonnect-Frontend/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d586706c27e37b866653635ab8c968fb",
+    "url": "/KareerKonnect-Frontend/static/media/signin-image.d586706c.jpg"
+  },
+  {
+    "revision": "75eb7c4e5d58a0a3f0d387b9bc44e080",
+    "url": "/KareerKonnect-Frontend/static/media/signup-image.75eb7c4e.jpg"
+  },
+  {
+    "revision": "d4a14f362b0d7c4cf277d7f46e8faf9c",
+    "url": "/KareerKonnect-Frontend/static/media/img1.d4a14f36.jpg"
+  },
+  {
+    "revision": "a357c3b2b2dc98903869f51261a5de1e",
+    "url": "/KareerKonnect-Frontend/static/media/p1.a357c3b2.jpg"
+  },
+  {
+    "revision": "1a1ff9d5b92e773fbd5d9620d4d0572b",
+    "url": "/KareerKonnect-Frontend/static/media/p2.1a1ff9d5.jpg"
+  },
+  {
+    "revision": "3fb1c505b1d8550af99d5c3197b09bf8",
+    "url": "/KareerKonnect-Frontend/static/media/p4.3fb1c505.jpg"
+  },
+  {
+    "revision": "7719d803d6eee86c9b9fa5ca9538edfb",
+    "url": "/KareerKonnect-Frontend/static/media/image.7719d803.jpg"
+  },
+  {
+    "revision": "d09a82a23891a40fb7bff51d9183c904",
+    "url": "/KareerKonnect-Frontend/static/media/image.d09a82a2.jpg"
+  },
+  {
+    "revision": "a7681279a3099ef6cfa91f868f884f00",
+    "url": "/KareerKonnect-Frontend/index.html"
+  }
+];
